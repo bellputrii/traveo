9a0c97c6c43de9f43a8216ddce9723c8ed682f7b
bellputrii <beldaputripramono@mail.ugm.ac.id>
2025-10-31 19:57:02 +0700

    feat: fetch api login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
     try {
       if (isLogin) {
         // 🔹 Login logic
-        const response = await fetch("http://localhost:3001/api/v1/login", {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
           method: "POST",
           headers: { 
             "Content-Type": "application/x-www-form-urlencoded",
